cf6564c4bede334198bae9d538448f04140b1ad3
andriyhrishin <agrishin@gmail.com>
2018-11-30 19:58:29 +0000

    WorkerMDB that connects to remote master JMS queue

--- src/main/java/hello/WorkerMDB.java
+++ src/main/java/hello/WorkerMDB.java
@@ -13,12 +13,22 @@ import javax.jms.TextMessage;
  * A simple Message Driven Bean that asynchronously receives and processes the messages that are sent to the queue.
  * </p>
  *
- * @author Serge Pagop (spagop@redhat.com)
  */
+/*
 @MessageDriven(name = "WorkerMDB", activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/test"),
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
         @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
+*/
+@MessageDriven(name = "WorkerMDB", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "testQueue"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+        @ActivationConfigProperty(propertyName = "user", propertyValue = "quickstartUser"),
+        @ActivationConfigProperty(propertyName = "password", propertyValue = "quickstartPwd1!"),
+        @ActivationConfigProperty(propertyName = "connectionParameters", propertyValue = "host=jbossm;port=5445"),
+        @ActivationConfigProperty(propertyName = "connectorClassName", propertyValue = "org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory")
+}, mappedName = "testQueue")
 public class WorkerMDB  implements MessageListener {
 
     private static final Logger LOGGER = Logger.getLogger(WorkerMDB.class.toString());
@@ -37,6 +47,11 @@ public class WorkerMDB  implements MessageListener {
             }
         } catch (JMSException e) {
             throw new RuntimeException(e);
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            LOGGER.info("InterruptedException: " + e.getMessage());
         }
     }
 }
